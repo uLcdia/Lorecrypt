e9e3e5f696dde4b3bd792ef4c39675065b5f4afb
Max Caulfield <ulctrix@outlook.com>
2024-07-07 16:52:31 +0800

    Complete Part01_07.MessageThreeTimes

--- src/tmcdata/mooc-java-programming-i/part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -7,7 +7,14 @@ public class MessageThreeTimes {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write a message:");
-        // Write your program here
+        
+        String message = scanner.nextLine();
+
+        for(int i = 0; i < 3; ++i) {
+            System.out.println(message);
+        }
+
+        scanner.close();
 
     }
 }
