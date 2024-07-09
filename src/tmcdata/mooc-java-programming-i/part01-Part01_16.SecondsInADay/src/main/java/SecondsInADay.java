a722c7ab20a12acf85a02addffed2f29e1644563
Max Caulfield <ulctrix@outlook.com>
2024-07-09 17:37:40 +0800

    Complete Part01_16.SecondsInADay

--- src/tmcdata/mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 
 public class SecondsInADay {
 
+    private static class Constants {
+        public static final int HOURS_IN_DAY = 24;
+        public static final int MINUTES_IN_HOUR = 60;
+        public static final int SECONDS_IN_MINUTE = 60;
+    }
+
+    public static int calculateSeconds(int days) {
+        return days * Constants.HOURS_IN_DAY * Constants.MINUTES_IN_HOUR * Constants.SECONDS_IN_MINUTE;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+
+        int days = scanner.nextInt();
+
+        System.out.println(calculateSeconds(days));
+        
+        scanner.close();
 
     }
 }
