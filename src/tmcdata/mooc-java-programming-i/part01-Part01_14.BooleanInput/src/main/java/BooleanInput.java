16b071edc9afa9acf847552797fd2213605e66d7
Max Caulfield <ulctrix@outlook.com>
2024-07-09 10:21:27 +0800

    Complete Part01_14.BooleanInput

--- src/tmcdata/mooc-java-programming-i/part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -6,7 +6,13 @@ public class BooleanInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
+        System.out.println("Write something:");
+
+        boolean verification = Boolean.parseBoolean(scanner.nextLine());
+
+        System.out.println("True or false? " + verification);
+
+        scanner.close();
 
     }
 }
