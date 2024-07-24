82aea1a68dc484e7a10fbe031ecc3b579475b135
Max Caulfield <ulctrix@outlook.com>
2024-07-24 15:27:42 +0800

    Complete Part01_26.Orwell

--- src/tmcdata/mooc-java-programming-i/part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -6,6 +6,12 @@ public class Orwell {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give a number:");
+
+        if (Integer.parseInt(scan.nextLine()) == 1984) {
+            System.out.println("Orwell");
+        }
+
+        scan.close();
     }
 }
