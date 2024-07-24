a4d31ef76bd9778f0ffcbca3f3bc90368c648baf
Max Caulfield <ulctrix@outlook.com>
2024-07-24 09:55:30 +0800

    Complete Part01_22.AverageOfThreeNumbers

--- src/tmcdata/mooc-java-programming-i/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -6,7 +6,18 @@ public class AverageOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The average is " + (first + second + third) / 3.0);
+
+        scanner.close();
 
     }
 }
