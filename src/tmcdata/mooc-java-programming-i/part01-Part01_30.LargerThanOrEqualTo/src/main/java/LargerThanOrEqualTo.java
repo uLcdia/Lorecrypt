843088dc967ae0ea184de1bb751f9ebff3bb7a93
Max Caulfield <ulctrix@outlook.com>
2024-07-24 17:18:31 +0800

    Complete Part01_30.LargerThanOrEqualTo

--- src/tmcdata/mooc-java-programming-i/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -6,5 +6,21 @@ public class LargerThanOrEqualTo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.parseInt(scan.nextLine());
+
+        System.out.println("Give the second  number:");
+        int secondNumber = Integer.parseInt(scan.nextLine());
+
+        if (firstNumber < secondNumber) {
+            System.out.println("Greater number is: " + secondNumber);
+        } else if (firstNumber > secondNumber) {
+            System.out.println("Greater number is: " + firstNumber);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
+
+        scan.close();
+
     }
 }
