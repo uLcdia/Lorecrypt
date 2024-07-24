f323d02fc338a3dab8bf1fb271c20d6bca5aa72c
Max Caulfield <ulctrix@outlook.com>
2024-07-24 20:50:03 +0800

    Complete Part01_34.Same

--- src/tmcdata/mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,18 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Password?");
+
+        String string1 = scan.nextLine();
+        String string2 = scan.nextLine();
+
+        if (string1.equals(string2)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+
+        scan.close();
+
     }
 }
