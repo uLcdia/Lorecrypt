4991eedc21df716ea7b5d5b4501d36d7aec2d557
Max Caulfield <ulctrix@outlook.com>
2024-07-24 09:57:46 +0800

    Complete Part01_23.SimpleCalculator

--- src/tmcdata/mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -6,7 +6,18 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(first + " + " + second + " = " + (first + second));
+        System.out.println(first + " - " + second + " = " + (first - second));
+        System.out.println(first + " * " + second + " = " + (first * second));
+        System.out.println(first + " / " + second + " = " + ((double) first / second));
+
+        scanner.close();
 
     }
 }
