07c287f0d7f763f7565ff55214b0d567bcba9eb1
Max Caulfield <ulctrix@outlook.com>
2024-07-24 10:24:20 +0800

    Complete Part01_24.SpeedingTicket

--- src/tmcdata/mooc-java-programming-i/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -6,6 +6,12 @@ public class SpeedingTicket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Give speed:");
+
+        if (Integer.parseInt(scanner.nextLine()) > 120) {
+            System.out.println("Speeding ticket!");
+        }
+
+        scanner.close();
     }
 }
