62084a424f0df19e3d754d43ef7e4c8675cdc566
Max Caulfield <ulctrix@outlook.com>
2024-07-24 17:15:31 +0800

    Complete Part01_29.Adulthood

--- src/tmcdata/mooc-java-programming-i/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -6,6 +6,14 @@ public class Adulthood {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        System.out.println("How old are you?");
+
+        if (Integer.parseInt(scan.nextLine()) < 18) {
+            System.out.println("You are not an adult");
+        } else {
+            System.out.println("You are an adult");
+        }
+
+        scan.close();
     }
 }
