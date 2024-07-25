24f0a2c734e2e6b7ad7c77fa4583132b1dd8ef85
Max Caulfield <ulctrix@outlook.com>
2024-07-25 20:11:11 +0800

    Complete Part01_35.CheckingTheAge

--- src/tmcdata/mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -6,5 +6,15 @@ public class CheckingTheAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int age = Integer.parseInt(scan.nextLine());
+
+        if (0 <= age && age <= 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+
+        scan.close();
+
     }
 }
