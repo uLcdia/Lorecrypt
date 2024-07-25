ca1a9e80d38c5f62f31ee1825c4eb0720b3b81a2
Max Caulfield <ulctrix@outlook.com>
2024-07-25 20:24:33 +0800

    Complete Part01_36.LeapYear

--- src/tmcdata/mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ src/tmcdata/mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -6,5 +6,21 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int year = Integer.parseInt(scan.nextLine());
+
+        if (year % 25 == 0) {
+            if (year % 16 == 0) {
+                System.out.println("The year is a leap year.");
+            } else {
+                System.out.println("The year is not a leap year.");
+            }
+        } else if (year % 4 == 0) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
+
+        scan.close();
+
     }
 }
