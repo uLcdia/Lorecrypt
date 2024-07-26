38566c81a6dbe03ae251cc4f2c6310d4f4b68ba6
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:17:09 +0800

    Complete Part02_13.AverageOfPositiveNumbers

--- src/tmcdata/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,29 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number > 0) {
+                count++;
+                sum += number;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double) sum / count);
+        }
+
+        scanner.close();
+
     }
 }
