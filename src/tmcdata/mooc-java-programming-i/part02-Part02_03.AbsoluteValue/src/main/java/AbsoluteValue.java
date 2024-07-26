0e148f8c6186a6042f77a965b2592f085e3017ce
Max Caulfield <ulctrix@outlook.com>
2024-07-26 20:47:30 +0800

    Complete Part02_03.AbsoluteValue

--- src/tmcdata/mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,9 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println(Math.abs(Integer.parseInt(scanner.nextLine())));
+
+        scanner.close();
+
     }
 }
