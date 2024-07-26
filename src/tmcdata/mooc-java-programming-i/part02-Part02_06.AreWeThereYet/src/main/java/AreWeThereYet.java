3460cd83515e39599ded570e1536b151e1312139
Max Caulfield <ulctrix@outlook.com>
2024-07-26 20:55:18 +0800

    Complete Part02_06.AreWeThereYet

--- src/tmcdata/mooc-java-programming-i/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -6,5 +6,11 @@ public class AreWeThereYet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        do {
+            System.out.println("Give a number:");
+        } while (!scanner.nextLine().equals("4"));
+
+        scanner.close();
+
     }
 }
