68d16af3fc20740f782e09c40d5a83ad9c66c0c8
Max Caulfield <ulctrix@outlook.com>
2024-07-26 22:18:52 +0800

    Complete Part02_17.SumOfASequence

--- src/tmcdata/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,16 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int lastNumber = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = 1; i <= lastNumber; ++i) {
+            sum += i;
+        }
+
+        System.out.println("The sum is " + sum);
+
+        scanner.close();
+
     }
 }
