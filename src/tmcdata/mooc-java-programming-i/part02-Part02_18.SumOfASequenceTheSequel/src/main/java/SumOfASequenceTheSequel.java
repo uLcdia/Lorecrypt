08500145c9d2168017c05a112dee3f5f4240566b
Max Caulfield <ulctrix@outlook.com>
2024-07-26 22:20:23 +0800

    Complete Part02_18.SumOfASequenceTheSequel

--- src/tmcdata/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,21 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("First number? ");
+        int beginning = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Last number? ");
+        int end = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+        for (int i = beginning; i <= end; ++i) {
+            sum += i;
+        }
+
+        System.out.println("The sum is " + sum);
+
+        scanner.close();
+
     }
 }
