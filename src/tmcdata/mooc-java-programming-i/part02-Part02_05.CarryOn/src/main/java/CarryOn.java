fc42dc890a94872418d1597241cc58be81dc5aa8
Max Caulfield <ulctrix@outlook.com>
2024-07-26 20:53:14 +0800

    Complete Part02_05.CarryOn

--- src/tmcdata/mooc-java-programming-i/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -6,5 +6,11 @@ public class CarryOn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        do {
+            System.out.println("Shall we carry on?");
+        } while (!scanner.nextLine().equals("no"));
+
+        scanner.close();
+
     }
 }
