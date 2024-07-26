f3422391f6a7a3aa1b29a0bca0da7b882fe9b575
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:36:35 +0800

    Complete Part02_15.CountingToHundred

--- src/tmcdata/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,13 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int beginning = Integer.parseInt(scanner.nextLine());
+
+        for (int i = beginning; i <= 100; ++i) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+
     }
 }
