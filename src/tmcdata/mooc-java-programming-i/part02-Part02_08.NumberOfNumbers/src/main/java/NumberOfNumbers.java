d02fbd0c0e299d43e925af75e5c5b3a708ee19a4
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:05:00 +0800

    Complete Part02_08.NumberOfNumbers

--- src/tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,20 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            if (Integer.parseInt(scanner.nextLine()) != 0) {
+                ++count;
+            } else {
+                System.out.println("Number of numbers: " + count);
+                break;
+            }
+        }
+
+        scanner.close();
+
     }
 }
