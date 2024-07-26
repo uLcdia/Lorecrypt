15dd4a6cf165a3e882d352deb1850f90fbf160b5
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:14:44 +0800

    Complete Part02_12.AverageOfNumbers

--- src/tmcdata/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,25 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            count++;
+            sum += number;
+        }
+
+        System.out.println("Average of the numbers: " + (double) sum / count);
+
+        scanner.close();
+
     }
 }
