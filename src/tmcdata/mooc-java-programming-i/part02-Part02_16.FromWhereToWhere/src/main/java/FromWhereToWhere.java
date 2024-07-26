d01c65c72099a9ef52371ea094d1bd7826397e79
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:41:00 +0800

    Complete Part02_16.FromWhereToWhere

--- src/tmcdata/mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,17 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Where to? ");
+        int end = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Where from? ");
+        int beginning = Integer.parseInt(scanner.nextLine());
+
+        for (int i = beginning; i <= end; ++i) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+
     }
 }
