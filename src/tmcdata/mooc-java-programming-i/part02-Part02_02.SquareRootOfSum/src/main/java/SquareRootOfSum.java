d948ac6e76e7b89f3a5845c0497c855050554853
Max Caulfield <ulctrix@outlook.com>
2024-07-26 20:46:16 +0800

    Complete Part02_02.SquareRootOfSum

--- src/tmcdata/mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,12 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number1 = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(Math.sqrt(number1 + number2));
+
+        scanner.close();
+
     }
 }
