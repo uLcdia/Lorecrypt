65c69494199e3c66cc7986a30c73eb4d57d7bf1d
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:08:49 +0800

    Complete Part02_09.NumberOfNegativeNumbers

--- src/tmcdata/mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,5 +6,25 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int negativeCount = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if (number < 0) {
+                ++negativeCount;
+            }
+
+            if (number == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Number of negative numbers: " + negativeCount);
+
+        scanner.close();
+
     }
 }
