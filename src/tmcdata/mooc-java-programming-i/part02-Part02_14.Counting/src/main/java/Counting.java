d25c4d234739712809eb7dbbf5569bb994899f0a
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:35:26 +0800

    Complete Part02_14.Counting

--- src/tmcdata/mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -6,5 +6,13 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i <= count; ++i) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+
     }
 }
