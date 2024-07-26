6517c12d32aaedd676b53586a655fade73849a7c
Max Caulfield <ulctrix@outlook.com>
2024-07-26 21:11:07 +0800

    Complete Part02_10.SumOfNumbers

--- src/tmcdata/mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -6,5 +6,23 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int number = Integer.parseInt(scanner.nextLine());
+
+            sum += number;
+
+            if (number == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Sum of the numbers: " + sum);
+
+        scanner.close();
+
     }
 }
