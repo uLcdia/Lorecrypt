46dc2243607adb2aea7b15d940a6baac1803eef8
Max Caulfield <ulctrix@outlook.com>
2024-07-26 22:23:38 +0800

    Complete Part02_19.Factorial

--- src/tmcdata/mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,18 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number: ");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int factorial = 1;
+
+        for (int i = 1; i <= number; ++i) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial: " + factorial);
+
+        scanner.close();
+
     }
 }
