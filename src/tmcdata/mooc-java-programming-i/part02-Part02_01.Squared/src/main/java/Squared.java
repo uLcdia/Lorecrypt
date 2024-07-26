6ede9ad0a57decf7f5b7465ff90409756c6cb012
Max Caulfield <ulctrix@outlook.com>
2024-07-26 20:44:40 +0800

    Complete Part02_01.Squared

--- src/tmcdata/mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,11 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(number * number);
+        
+        scanner.close();
+
     }
 }
