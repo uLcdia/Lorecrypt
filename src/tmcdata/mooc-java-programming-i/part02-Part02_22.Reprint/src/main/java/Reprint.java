400e3fbc8c818a6a359e75f73c502c7bf58a7534
uLcdia <ulctrix@outlook.com>
2024-08-04 18:24:40 +0800

    Complete Part02_22.Reprint

--- src/tmcdata/mooc-java-programming-i/part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -5,14 +5,22 @@ public class Reprint {
 
     public static void main(String[] args) {
         // ask the user for how many times should the text be printed
-        // then call the printText-method multiple times with a while-loop
-        
+        System.out.println("How many times?");
         Scanner scanner = new Scanner(System.in);
+        int times = Integer.valueOf(scanner.nextLine());
+
+        // then call the printText-method multiple times with a while-loop
+        for (int i = 0; i < times; ++i) {
+            printText();
+        }
+
+        scanner.close();
+        
     }
     
     // Don't change the next line that defines the method!
     // (We aren't giving a method a parameter yet)
     public static void printText() {
-        // write some code here
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
