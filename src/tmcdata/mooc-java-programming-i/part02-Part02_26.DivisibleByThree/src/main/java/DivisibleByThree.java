167672202b22057b6bdee75685205ef96acdd9d3
uLcdia <ulctrix@outlook.com>
2024-08-04 19:34:13 +0800

    Complete Part02_26.DivisibleByThree

--- src/tmcdata/mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -1,11 +1,13 @@
-
-import java.util.Scanner;
-
 public class DivisibleByThree {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        divisibleByThreeInRange(9, 12);
+    }
 
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = (beginning + ((beginning % 3 == 0) ? 0 : 3 - beginning % 3)); i <= end; i += 3) {
+            System.out.println(i);
+        }
     }
 
 }
