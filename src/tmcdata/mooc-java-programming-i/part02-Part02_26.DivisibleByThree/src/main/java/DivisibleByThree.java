5099b524507e3d8632bfddf77c27d2b9cfa512af
uLcdia <ulctrix@outlook.com>
2024-08-10 10:20:50 +0800

    Modify Part02_26.DivisibleByThree

--- src/tmcdata/mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ src/tmcdata/mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -3,9 +3,10 @@ public class DivisibleByThree {
     public static void main(String[] args) {
         divisibleByThreeInRange(9, 12);
     }
-
+    
     public static void divisibleByThreeInRange(int beginning, int end) {
-        for (int i = (beginning + ((beginning % 3 == 0) ? 0 : 3 - beginning % 3)); i <= end; i += 3) {
+        int start = beginning + (3 - beginning % 3) % 3;
+        for (int i = start; i <= end; i += 3) {
             System.out.println(i);
         }
     }
