01e9e05326e676c6691defd62f9aed65c9b5c7d6
uLcdia <ulctrix@outlook.com>
2024-09-05 13:33:45 +0800

    Complete Part03_02.SecondPlusThird

--- src/tmcdata/mooc-java-programming-i/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
@@ -17,6 +17,8 @@ public class SecondPlusThird {
             numbers.add(number);
         }
 
-        System.out.println(luvut.get(0));
+        System.out.println(numbers.get(1) + numbers.get(2));
+
+        scanner.close();
     }
 }
