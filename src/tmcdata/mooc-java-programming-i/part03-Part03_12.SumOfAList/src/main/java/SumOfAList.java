9adfb462457a319e14eee2c1f454cc47510237d3
uLcdia <ulctrix@outlook.com>
2024-09-06 15:13:35 +0800

    Complete Part03_12.SumOfAList

--- src/tmcdata/mooc-java-programming-i/part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
@@ -7,6 +7,7 @@ public class SumOfAList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
         ArrayList<Integer> list = new ArrayList<>();
         while (true) {
             int input = Integer.valueOf(scanner.nextLine());
@@ -15,10 +16,9 @@ public class SumOfAList {
             }
 
             list.add(input);
+            sum += input;
         }
 
-        System.out.println("");
-
-        // toteuta listan lukujen summan laskeminen tänne
+        System.out.println("Sum: " + sum);
     }
 }
