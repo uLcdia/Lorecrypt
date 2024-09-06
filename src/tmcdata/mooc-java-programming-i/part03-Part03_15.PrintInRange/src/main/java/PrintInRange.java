ae70a0ef4dcd22f638ac16effcbc2ca01ace78e0
uLcdia <ulctrix@outlook.com>
2024-09-06 17:25:29 +0800

    Complete Part03_15.PrintInRange

--- src/tmcdata/mooc-java-programming-i/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -6,5 +6,12 @@ public class PrintInRange {
     public static void main(String[] args) {
         // Try your method here
     }
-    
+
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int number : numbers) {
+            if (number >= lowerLimit && number <= upperLimit) {
+                System.out.println(number);
+            }
+        }
+    }
 }
