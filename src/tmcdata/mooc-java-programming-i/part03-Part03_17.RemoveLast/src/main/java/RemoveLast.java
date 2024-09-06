d081804d03f8c0b1a5d9a88634f1f845adc35cf5
uLcdia <ulctrix@outlook.com>
2024-09-06 17:36:35 +0800

    Complete Part03_17.RemoveLast

--- src/tmcdata/mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -7,4 +7,10 @@ public class RemoveLast {
         // Try your method in here
     }
 
+    public static void removeLast(ArrayList<String> strings) {
+        if (strings.size() != 0) {
+            strings.remove(strings.size() - 1); // still strings.removeLast() won't work
+        }
+    }
+
 }
