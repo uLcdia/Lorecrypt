00995b589347167d74eae51068b2a171c89e20a3
uLcdia <ulctrix@outlook.com>
2024-09-06 15:23:49 +0800

    Complete Part03_13.AverageOfAList

--- src/tmcdata/mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -7,10 +7,20 @@ public class AverageOfAList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // implement here a program, that first reads user input
-        // adding them on a list until user gives -1.
-        // Then it computes the average of the numbers on the list
-        // and prints it.
-        
+        int sum = 0;
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+            sum += input;
+            count++;
+        }
+
+        System.out.println("Average: " + (double)sum / count);
     }
 }
