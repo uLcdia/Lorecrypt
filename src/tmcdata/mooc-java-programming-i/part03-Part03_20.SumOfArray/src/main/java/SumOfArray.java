ab547f42bb649629a747ae6731e5791c090bda5f
uLcdia <ulctrix@outlook.com>
2024-09-07 11:57:31 +0800

    Complete Part03_20.SumOfArray

--- src/tmcdata/mooc-java-programming-i/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
@@ -1,4 +1,6 @@
 
+import java.util.Arrays;
+
 public class SumOfArray {
 
     public static void main(String[] args) {
@@ -9,6 +11,6 @@ public class SumOfArray {
 
     public static int sumOfNumbersInArray(int[] array) {
         // Write some code here
-        return 0;
+        return Arrays.stream(array).sum();
     }
 }
