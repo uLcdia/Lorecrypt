9d278e7e577a8ff2eab4aa0c4a6a2423f5743d9f
uLcdia <ulctrix@outlook.com>
2024-09-07 17:11:37 +0800

    Complete Part03_27.AVClub

--- src/tmcdata/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AVClub {
@@ -6,6 +7,16 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        do {
+            text = scanner.nextLine();
 
+            Arrays.stream(text.split(" "))
+                  .filter(word -> word.contains("av"))
+                  .forEach(System.out::println);
+
+        } while (text.length() > 0);
+
+        scanner.close();
     }
 }
