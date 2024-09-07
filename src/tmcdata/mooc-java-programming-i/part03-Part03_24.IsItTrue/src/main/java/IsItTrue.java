6837e4b8a693da6907d0dc54f30ef2fe7ec79a89
uLcdia <ulctrix@outlook.com>
2024-09-07 12:23:09 +0800

    Complete Part03_24.IsItTrue

--- src/tmcdata/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
@@ -6,5 +6,12 @@ public class IsItTrue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a string: ");
+        String word = scanner.nextLine();
+        
+        scanner.close();
+
+        System.out.println((word.equals("true")) ? "You got it right!" : "Try again!");
+
     }
 }
