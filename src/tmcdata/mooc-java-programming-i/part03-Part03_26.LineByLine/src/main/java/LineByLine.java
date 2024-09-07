f7cc8cfbd908e234abde688576785b6a415b4113
uLcdia <ulctrix@outlook.com>
2024-09-07 17:08:13 +0800

    Complete Part03_26.LineByLine

--- src/tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,11 +1,21 @@
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        do {
+            text = scanner.nextLine();
 
+            Arrays.stream(text.split(" "))
+                  .forEach(System.out::println);
+                  
+        } while (text.length() > 0);
+
+        scanner.close();
     }
 }
