33a0b129f197038ca3136315e4879c8ec03963a4
uLcdia <ulctrix@outlook.com>
2024-09-07 23:10:41 +0800

    Complete Part03_32.PersonalDetails

--- src/tmcdata/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,30 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        int count = 0;
+        int yearSum = 0;
+        int nameLengthMax = 0;
+        String name = "Neo";
+        do {
+            text = scanner.nextLine();
+            try {
+                yearSum += Integer.parseInt(text.split(",")[1]);
+                count++;
+            } catch(Exception e) {
+                System.err.println("Can't parse birth year with " + text);
+                continue;
+            }
 
+            if (text.split(",")[0].length() > nameLengthMax) {
+                nameLengthMax = text.split(",")[0].length();
+                name = text.split(",")[0];
+            }
+        } while (text.length() > 0);
+
+        scanner.close();
+
+        System.out.println("Longest name: " + name);
+        System.out.println("Average of the birth years: " + (double)yearSum / count);
     }
 }
