762e50e627761fbeabd880cca205e2df88e74b75
uLcdia <ulctrix@outlook.com>
2024-09-07 12:21:10 +0800

    Complete Part03_23.PrintThrice

--- src/tmcdata/mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 public class PrintThrice {
 
     public static void main(String[] args) {
+        final int REPEAT = 3;
+
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.print("Give a word: ");
+        String word = scanner.nextLine();
+        
+        scanner.close();
+
+        System.out.println(word.repeat(REPEAT));
 
     }
 }
