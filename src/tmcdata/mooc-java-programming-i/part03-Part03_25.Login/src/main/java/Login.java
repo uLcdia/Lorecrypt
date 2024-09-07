38ea794cbd9c9f6706af45da2bf94c1e21f99417
uLcdia <ulctrix@outlook.com>
2024-09-07 12:38:21 +0800

    Complete Part03_25.Login

--- src/tmcdata/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -1,10 +1,26 @@
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        HashMap<String, String> userCredentials = new HashMap<>();
+
+        userCredentials.put("alex", "sunshine");
+        userCredentials.put("emma", "haskell");
+
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+
+        if (userCredentials.containsKey(username) && userCredentials.get(username).equals(password)) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
