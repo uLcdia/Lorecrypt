57c790e9d24dc3d14f3ecd68cb32a6d13dc12f94
uLcdia <ulctrix@outlook.com>
2024-09-07 22:43:51 +0800

    Complete Part03_29.LastWords

--- src/tmcdata/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,16 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        do {
+            text = scanner.nextLine();
 
+            String[] words = text.split(" ");
+
+            System.out.println(words[words.length - 1]);
+
+        } while (text.length() > 0);
+
+        scanner.close();
     }
 }
