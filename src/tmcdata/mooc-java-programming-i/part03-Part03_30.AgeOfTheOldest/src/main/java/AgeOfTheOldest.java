ea5d6656fa5cc5d790c94eec47e2fe46cbdbd113
uLcdia <ulctrix@outlook.com>
2024-09-07 23:01:04 +0800

    Complete Part03_30.AgeOfTheOldest

--- src/tmcdata/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,23 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        int age;
+        int maxAge = -1;
+        do {
+            text = scanner.nextLine();
+            try {
+                age = Integer.parseInt(text.split(",")[1]);
+            } catch(Exception e) {
+                System.err.println("Can't parse age with " + text);
+                continue;
+            }
 
+            maxAge = Integer.max(age, maxAge);
+        } while (text.length() > 0);
+
+        scanner.close();
+
+        System.out.println("Age of the oldest: " + maxAge);
     }
 }
