ad97c89bbaf0ae6ec3807f90b9036df45683f706
uLcdia <ulctrix@outlook.com>
2024-09-07 23:04:12 +0800

    Complete Part03_31.NameOfTheOldest

--- src/tmcdata/mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,27 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        int age;
+        int maxAge = -1;
+        String oldestName = "The Oracle";
+        do {
+            text = scanner.nextLine();
+            try {
+                age = Integer.parseInt(text.split(",")[1]);
+            } catch(Exception e) {
+                System.err.println("Can't parse age with " + text);
+                continue;
+            }
 
+            if (age > maxAge) {
+                maxAge = age;
+                oldestName = text.split(",")[0];
+            }
+        } while (text.length() > 0);
+
+        scanner.close();
+
+        System.out.println("Name of the oldest: " + oldestName);
     }
 }
