88d573989eaf7b8ef7f54bd683316aa2371606af
uLcdia <ulctrix@outlook.com>
2024-09-07 22:39:04 +0800

    Complete Part03_28.FirstWords

--- src/tmcdata/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ src/tmcdata/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,14 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String text;
+        do {
+            text = scanner.nextLine();
 
+            System.out.println(text.split(" ")[0]);
+
+        } while (text.length() > 0);
+
+        scanner.close();
     }
 }
