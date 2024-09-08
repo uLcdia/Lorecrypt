e0713cd00ac5e10a0b6c11a28a436f83390126b2
uLcdia <ulctrix@outlook.com>
2024-09-08 22:55:55 +0800

    Complete Part04_01.YourFirstAccount

--- src/tmcdata/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,12 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+
+        // You'll need jdk11 from now on to run the TMC test
+
+        Account myAccount = new Account("Max Caulfield", 100);
+        myAccount.deposit(20);
+        System.out.println(myAccount);
+
     }
 }
