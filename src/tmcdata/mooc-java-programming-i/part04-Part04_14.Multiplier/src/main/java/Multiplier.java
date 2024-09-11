21af2287b2ba3d5bdedae182c4851414c125b6cc
uLcdia <ulctrix@outlook.com>
2024-09-11 21:26:18 +0800

    Complete Part04_14.Multiplier

---
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,11 @@
+public class Multiplier {
+    private int multiplier;
+
+    public Multiplier(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public int multiply(int number) {
+        return multiplier * number;
+    }
+}
