e1f4d3d4ea051482c8477059048fa62d0060c990
uLcdia <ulctrix@outlook.com>
2024-09-11 21:51:37 +0800

    Complete Part04_15.NumberStatistics

--- src/tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,30 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+
+        Statistics statistics = new Statistics();
+        Statistics evenSstaStatistics = new Statistics();
+        Statistics oddStatistics = new Statistics();
+
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+
+            if (number % 2 == 0) {
+                evenSstaStatistics.addNumber(number);
+            } else {
+                oddStatistics.addNumber(number);
+            }
+            statistics.addNumber(number);
+        }
+        scanner.close();
+
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + evenSstaStatistics.sum());
+        System.out.println("Sum of odd numbers: " + oddStatistics.sum());
     }
 }
