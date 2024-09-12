29fa0776316e2457bb35ce0ec8fea78ee9e4c129
uLcdia <ulctrix@outlook.com>
2024-09-12 17:30:30 +0800

    Complete Part04_18.PersonalInformation

--- src/tmcdata/mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,27 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("First name: ");
+            String firstName = scanner.nextLine();
+            if (firstName.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.println("Identification number: ");
+            String idNumber = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNumber));
+        }
+
+        scanner.close();
+
+        for (PersonalInformation person : infoCollection) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
+
     }
 }
