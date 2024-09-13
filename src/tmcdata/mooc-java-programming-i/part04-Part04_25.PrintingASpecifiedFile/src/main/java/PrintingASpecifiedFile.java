523ec87019d6b17ef29368a9f7522f05cfeac3b1
uLcdia <ulctrix@outlook.com>
2024-09-13 14:01:41 +0800

    Complete Part04_25.PrintingASpecifiedFile

--- src/tmcdata/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,11 +1,26 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.io.IOException;
 
 public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which file should have its contents printed?");
+
+        String fileName = scanner.nextLine();
+
+        scanner.close();
+
+        try {
+            Files.lines(Paths.get(fileName))
+                 .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
