4a4400843d8bec7ff93b399b7a5f43f59e320140
uLcdia <ulctrix@outlook.com>
2024-09-13 15:16:37 +0800

    Complete Part04_29.RecordsFromAFile

--- src/tmcdata/mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,5 +1,6 @@
 
 import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class RecordsFromAFile {
@@ -7,5 +8,22 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file:");
+
+        String fileName = scanner.nextLine();
+
+        scanner.close();
+
+        try {
+            Files.lines(Paths.get(fileName))
+                 .map(line -> line.split(","))
+                 .forEach(parts -> {
+                    String name = parts[0].trim();
+                    String ageString = parts[1].trim();
+                    System.out.println(name + ", age: " + ageString + " years");
+                 });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
