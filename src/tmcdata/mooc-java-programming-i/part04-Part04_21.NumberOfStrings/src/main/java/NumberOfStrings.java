347463f0fcf2fa24e9e6b52b7ccd7f40495449d1
uLcdia <ulctrix@outlook.com>
2024-09-13 13:26:42 +0800

    Complete Part04_21.NumberOfStrings

--- src/tmcdata/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -6,5 +6,19 @@ public class NumberOfStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+
+        while (true) {
+            if (!scanner.nextLine().equals("end")) {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        scanner.close();
+
+        System.out.println(count);
+
     }
 }
