10039285c4210849b4997fc3c2c682dccda27883
uLcdia <ulctrix@outlook.com>
2024-09-13 13:32:34 +0800

    Complete Part04_22.Cubes

--- src/tmcdata/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -1,10 +1,25 @@
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        final int EXPONENT = 3;
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (!input.equals("end")) {
+                System.out.println((int)Math.pow(Integer.parseInt(input), EXPONENT));
+            } else {
+                break;
+            }
+        }
+
+        scanner.close();
 
     }
 }
