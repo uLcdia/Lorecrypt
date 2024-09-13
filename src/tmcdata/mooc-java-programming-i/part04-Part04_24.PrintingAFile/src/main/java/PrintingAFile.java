4da8c839df994e8ac0c9ce093a6ba580398d81c9
uLcdia <ulctrix@outlook.com>
2024-09-13 13:59:03 +0800

    Complete Part04_24.PrintingAFile

--- src/tmcdata/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ src/tmcdata/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -1,10 +1,17 @@
 
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.io.IOException;
 
 public class PrintingAFile {
 
     public static void main(String[] args) {
+        try {
+            Files.lines(Paths.get("data.txt"))
+                 .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
