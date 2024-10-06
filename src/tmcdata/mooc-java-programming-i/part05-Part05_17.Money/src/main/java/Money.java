608d851b22c39b0841d3907f6e802138c564608d
uLcdia <ulctrix@outlook.com>
2024-10-06 16:20:27 +0800

    Complete Part05_17.Money

--- src/tmcdata/mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ src/tmcdata/mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -10,9 +10,19 @@ public class Money {
             euros = euros + cents / 100;
             cents = cents % 100;
         }
+        
+        if (cents < 0) {
+            euros = euros - 1;
+            cents = cents + 100;
+        }
 
-        this.euros = euros;
-        this.cents = cents;
+        if (euros < 0) {
+            this.euros = 0;
+            this.cents = 0;
+        } else {
+            this.euros = euros;
+            this.cents = cents;
+        }
     }
 
     public int euros() {
@@ -32,4 +42,23 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros + addition.euros, this.cents + addition.cents);
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        }
+        if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+        }
+        return false;
+    }
+
+    public Money minus(Money subtraction) {
+        Money newMoney = new Money(this.euros - subtraction.euros, this.cents - subtraction.cents);
+        return newMoney;
+    }
 }
