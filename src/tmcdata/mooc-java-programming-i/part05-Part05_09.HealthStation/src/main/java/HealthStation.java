da021b93fae6075753be801610d00544f7f50185
uLcdia <ulctrix@outlook.com>
2024-10-06 12:25:51 +0800

    Complete Part05_09.HealthStation

--- src/tmcdata/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ src/tmcdata/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,24 @@
 
 public class HealthStation {
-
+    private int weighingCount = 0;
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
+        if (person != null) {
+            this.weighingCount++;
+            return person.getWeight();
+        }
         return -1;
+    }
+
+    public void feed(Person person) {
+        if (person != null) {
+            person.setWeight(person.getWeight() + 1);
+        }
+    }
+
+    public int weighings() {
+        return this.weighingCount;
     }
 
 }
