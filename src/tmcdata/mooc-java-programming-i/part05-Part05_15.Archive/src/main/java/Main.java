b47f7f81621a630a8cd97447d2e8526186d16dcc
uLcdia <ulctrix@outlook.com>
2024-10-06 15:40:44 +0800

    Complete Part05_15.Archive

--- src/tmcdata/mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
+++ src/tmcdata/mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
@@ -1,12 +1,32 @@
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        HashMap<String, String> archive = new HashMap<String, String>();
 
-
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String identifier = scanner.nextLine();
+            if (identifier.isEmpty()) {
+                break;
+            }
+            System.out.println("Name? (empty will stop)");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            if (!archive.containsKey(identifier)) {
+                archive.put(identifier, name);
+            }
+        }
+        scanner.close();
+        System.out.println("==Items==");
+        for (String key : archive.keySet()) {
+            System.out.println(key + ": " + archive.get(key));
+        }
     }
 }
