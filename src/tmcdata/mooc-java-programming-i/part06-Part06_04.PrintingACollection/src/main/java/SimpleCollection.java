1f3dc1f8feca86f49343cd4cc0921f88ae14cba4
uLcdia <ulctrix@outlook.com>
2024-10-10 18:13:10 +0800

    Complete Part06_04.PrintingACollection

--- src/tmcdata/mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ src/tmcdata/mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -1,5 +1,5 @@
-
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class SimpleCollection {
 
@@ -17,6 +17,20 @@ public class SimpleCollection {
 
     public ArrayList<String> getElements() {
         return this.elements;
+    }
+
+    @Override
+    public String toString() {
+        if (elements.isEmpty()) {
+            return "The collection " + name + " is empty.";
+        }
+        String elementsString = elements.stream()
+            .map(element -> element.toString())
+            .collect(Collectors.joining("\n"));
+        if (elements.size() == 1) {
+            return "The collection " + name + " has " + elements.size() + " element:\n" + elementsString;
+        }
+        return "The collection " + name + " has " + elements.size() + " elements:\n" + elementsString;
     }
     
 }
