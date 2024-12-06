6398e42d0f7fde01851c97be84fb16e11c2e91a4
uLcdia <49097072+uLcdia@users.noreply.github.com>
2024-12-06 17:25:19 +0800

    Complete Part07_02.LiquidContainers2

--- src/tmcdata/mooc-java-programming-i/part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ src/tmcdata/mooc-java-programming-i/part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -2,20 +2,58 @@
 import java.util.Scanner;
 
 public class LiquidContainers2 {
+    private static Container firstContainer = new Container();
+    private static Container secondContainer = new Container();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
         while (true) {
-            System.out.print("> ");
+            print();
 
             String input = scan.nextLine();
-            if (input.equals("quit")) {
-                break;
+            String[] parts = input.split(" ");
+
+            try {
+                String command = parts[0];
+                int amount = Integer.valueOf(parts[1]);
+                if (command.equals("add")) {
+                    add(amount);
+                } else if (command.equals("move")) {
+                    move(amount);
+                } else if (command.equals("remove")) {
+                    remove(amount);
+                } else {
+                    System.out.println("Unknown command");
+                }
+            } catch (IndexOutOfBoundsException e) {
+                if (input.equals("quit")) {
+                    break;
+                }
+                System.out.println("Please provide a command and an amount.");
+                continue;
+            } catch (NumberFormatException e) {
+                System.out.println("Amount must be an integer.");
+                continue;
             }
 
         }
     }
 
+    private static void add(int amount) {
+        firstContainer.add(amount);
+    }
+
+    private static void move(int amount) {
+        firstContainer.move(secondContainer, amount);
+    }
+
+    private static void remove(int amount) {
+        secondContainer.remove(amount);
+    }
+
+    private static void print() {
+        System.out.println("First: " + firstContainer);
+        System.out.println("Second: " + secondContainer);
+    }
 }
