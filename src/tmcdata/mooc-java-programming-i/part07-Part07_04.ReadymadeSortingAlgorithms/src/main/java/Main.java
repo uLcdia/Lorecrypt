6df2bd7fa87e73d613dddaa6500e9f4a33c03687
uLcdia <49097072+uLcdia@users.noreply.github.com>
2024-12-06 18:03:00 +0800

    Complete Part07_04.ReadymadeSortingAlgorithms

--- src/tmcdata/mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ src/tmcdata/mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
@@ -7,4 +8,19 @@ public class Main {
         int[] array = {3, 1, 5, 99, 3, 12};
     }
 
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        integers.sort(null);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        strings.sort(null);
+    }
 }
