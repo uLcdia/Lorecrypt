b706df8d740e6a5f23f390093c5e32bd7db71754
uLcdia <49097072+uLcdia@users.noreply.github.com>
2024-12-10 21:29:07 +0800

    Complete Part08_08.PrintMeMyHashmap

--- src/tmcdata/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ src/tmcdata/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -7,4 +7,24 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printKeys(HashMap<String,String> hashmap) {
+        hashmap.keySet()
+                .stream()
+                .forEach(System.out::println);
+    }
+
+    public static void printKeysWhere(HashMap<String,String> hashmap, String text) {
+        hashmap.keySet()
+                .stream()
+                .filter(key -> key.contains(text))
+                .forEach(System.out::println);
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text) {
+        hashmap.keySet()
+                .stream()
+                .filter(key -> key.contains(text))
+                .forEach(key -> System.out.println(hashmap.get(key)));
+    }
+    
 }
