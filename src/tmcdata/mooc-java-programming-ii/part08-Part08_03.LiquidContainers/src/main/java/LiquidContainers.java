83b9dd1d7dc70c4254ea2fa5bb82231ffe4d70b6
uLcdia <49097072+uLcdia@users.noreply.github.com>
2024-12-10 20:09:22 +0800

    Complete Part08_03.LiquidContainers

--- src/tmcdata/mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ src/tmcdata/mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -2,20 +2,69 @@
 import java.util.Scanner;
 
 public class LiquidContainers {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        final int MAX_AMOUNT = 100;
+
+        int firstContainer = 0;
+        int secondContainer = 0;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + firstContainer + "/" + MAX_AMOUNT);
+            System.out.println("Second: " + secondContainer + "/" + MAX_AMOUNT);
 
             String input = scan.nextLine();
-            if (input.equals("quit")) {
-                break;
+            String[] parts = input.split(" ");
+
+            try {
+                String command = parts[0];
+                int amount = Integer.valueOf(parts[1]);
+                if (command.equals("add")) {
+                    if (amount > 0) {
+                        int amountSum = firstContainer + amount;
+                        if (amountSum > MAX_AMOUNT) {
+                            amountSum = MAX_AMOUNT;
+                        }
+                        firstContainer = amountSum;
+                    }
+                } else if (command.equals("move")) {
+                    int amountMoved = 0;
+            
+                    if (firstContainer - amount < 0) {
+                        amountMoved = firstContainer;
+                        firstContainer = 0;
+                    } else {
+                        amountMoved = amount;
+                        firstContainer -= amount;
+                    }
+                    
+                    int amountSum = secondContainer + amountMoved;
+                    if (amountSum > MAX_AMOUNT) {
+                        amountSum = MAX_AMOUNT;
+                    }
+                    secondContainer = amountSum;
+                } else if (command.equals("remove")) {
+                    if (secondContainer - amount < 0) {
+                        secondContainer = 0;
+                    } else {
+                        secondContainer -= amount;
+                    }
+                } else {
+                    System.out.println("Unknown command");
+                }
+            } catch (IndexOutOfBoundsException e) {
+                if (input.equals("quit")) {
+                    break;
+                }
+                System.out.println("Please provide a command and an amount.");
+                continue;
+            } catch (NumberFormatException e) {
+                System.out.println("Amount must be an integer.");
+                continue;
             }
-
         }
-    }
 
+        scan.close();
+    }
 }
