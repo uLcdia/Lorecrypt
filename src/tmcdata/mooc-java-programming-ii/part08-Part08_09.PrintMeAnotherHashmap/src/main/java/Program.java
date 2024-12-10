4d15e5aa64649f432ee211ceb13b9a07cab2f662
uLcdia <49097072+uLcdia@users.noreply.github.com>
2024-12-10 21:36:55 +0800

    Complete Part08_09.PrintMeAnotherHashmap

--- src/tmcdata/mooc-java-programming-ii/part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ src/tmcdata/mooc-java-programming-ii/part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -7,4 +7,16 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printValues(HashMap<String,Book> hashmap) {
+        hashmap.values()
+                .stream()
+                .forEach(System.out::println);
+    }
+
+    public static void printValueIfNameContains(HashMap<String,Book> hashmap, String text) {
+        hashmap.values()
+                .stream()
+                .filter(book -> book.getName().contains(text))
+                .forEach(System.out::println);
+    }
 }
