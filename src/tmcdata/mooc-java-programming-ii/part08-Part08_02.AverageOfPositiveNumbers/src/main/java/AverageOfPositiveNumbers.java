2c8cefb050c2a70eb9cfd3825cae1ba1c92b49ab
uLcdia <49097072+uLcdia@users.noreply.github.com>
2024-12-10 20:05:04 +0800

    Complete Part08_02.AverageOfPositiveNumbers

--- src/tmcdata/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ src/tmcdata/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,30 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+            try {
+                int number = Integer.parseInt(input);
+                if (number > 0) {
+                    sum += number;
+                    count++;
+                } else if (number == 0) {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+            }
+        }
+
+        scanner.close();
+
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double) sum / count);
+        }
     }
 }
